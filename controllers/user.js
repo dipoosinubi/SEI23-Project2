f1bb5ea00f3d9af606448b1632bf373423b717cd
Oladipo Osinubi <diposinubi@gmail.com>
2019-08-26 12:10:25 -0400

    linked players to user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const teamsAPI = require('../models/team.js')
-// const playerAPI = require('../models/player.js')
+const playerAPI = require('../models/player.js')
 const usersAPI = require('../models/user.js')
 
 
@@ -47,55 +47,109 @@ userRouter.get('/new', function(req, res) {
   })
   // DELETE USER
   userRouter.delete('/:userId', function(req, res) {
-    usersAPI.deleteUsers(req.params.userId).then(res.redirect('/users'))
+    usersAPI.deleteUser  (req.params.userId).then(res.redirect('/users'))
   })
 
-  // ZZZZZZZZZZZZZZZ TESAM CONTROLLER ZZZZZZZZZZZZZ
+  // ZZZZZZZZZZZZZZZ TEAMS CONTROLLER ZZZZZZZZZZZZZ
   // ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
   //  GET ALL TEAMS
 userRouter.get('/:userId/teams', function (req, res) {
-  teamsAPI.getAllTeams().then(teams => {
-    res.render('teams/allTeams', {teams})
-  })
+  usersAPI.getUser(req.params.userId).then(user => 
+  // teamsAPI.getTeamsByUserId(user._id).then(teams => {
+    teamsAPI.getAllTeams().then (teams => {
+    res.render('teams/allTeams', {teams, user})
+  }))
 })
 
 // CREATE NEW TEAM
-userRouter.get('/:userId/teams/new', function(req, res) {
+userRouter.get('/:userId/newTeam', function(req, res) {
   res.render('teams/newTeam')
 })
 
-// // EDIT SINGLE TEAM
-// userRouter.get('/:teamId/editTeam', function(req, res) {
-//   teamsAPI.getTeam(req.params.teamId).then(team => {
-//     res.render('teams/editTeam', {team})
-//   })
-// })
+// EDIT SINGLE TEAM
+userRouter.get('/:teamId/editTeam', function(req, res) {
+  teamsAPI.getTeam(req.params.teamId).then(team => {
+    res.render('teams/editTeam', {team})
+  })
+})
  
-// // GET SINGLE TEAM
-// userRouter.get('/:teamId', function(req, res) {
-//   teamsAPI.getTeam(req.params.teamId).then (team =>{
-//     res.render('teams/team', {team})
-//   })
-// })
-// //ADD NEW TEAM
-// userRouter.post('/', function(req, res){
-//   teamsAPI.addNewTeam(req.body).then(() => {
-//     res.redirect('/teams');
-//   })
-// })
-// // UPDATE TEAM
-// userRouter.put('/:teamId', function (req, res){
-//   teamsAPI.updateTeam(req.params.teamId, req.body).then (
-//     () => {
-//       res.redirect('/teams');
-//   })
-// })
-// // DELETE TEAM
-// userRouter.delete('/:teamId', function(req, res) {
-//   teamsAPI.deleteTeam(req.params.teamId).then(res.redirect('/teams'))
-// })
+// GET SINGLE TEAM
+userRouter.get('/:teamId', function(req, res) {
+  teamsAPI.getTeam(req.params.teamId).then (team =>{
+    res.render('teams/team', {team})
+  })
+})
+//ADD NEW TEAM
+userRouter.post('/teams', function(req, res){
+  teamsAPI.addNewTeam(req.body).then(() => {
+    res.redirect('/users/:userId');
+  })
+})
+// UPDATE TEAM
+userRouter.put('/:teamId', function (req, res){
+  teamsAPI.updateTeam(req.params.teamId, req.body).then (
+    () => {
+      res.redirect('/teams');
+  })
+})
+// DELETE TEAM
+userRouter.delete('/:teamId', function(req, res) {
+  teamsAPI.deleteTeam(req.params.teamId).then(res.redirect('/teams'))
+})
+// ZZZZZZZZZZZZZZ PLAYER CONTROLLER ZZZZZZZZZZZZZZZZZZ
+// ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
 
+// GET PLAYERS BY TEAM
+// playerRouter.get('/team/:teamId/', function (req, res) {
+//   playerAPI.getPlayersByTeamId(req.params.teamId).then(player => {
+//     res.render('players/player', { player})
+//   })  
+// })
+// // GET ALL PLAYERS
+// playerRouter.get('/', function (req, res) {
+//   playerAPI.getAllPlayers().then(players => {
+//     res.render('players/allPlayers', { players })
+//   })
+// })
+// // CREATE PLAYER WITH TEAM ID
+// playerRouter.get('/team/:teamId/new', function (req, res) {
+//   res.render('players/newPlayer')
+// })
+// // EDIT SINGLE PLAYER
+// playerRouter.get('/:playerId/editPlayer', function (req, res) {
+//   playerAPI.getPlayer(req.params.playerId).then(player => {
+//     res.render('players/editPlayer', { player })
+//   })
+// })
+// // GET SINGLE PLAYER
+// playerRouter.get('/:playerId', function (req, res) {
+//   playerAPI.getPlayer(req.params.playerId)
+//     .then(player => {
+//       res.render('players/player', { player })
+//     })
+//     .catch(res.send)
 
+// })
+// // POST SINGLE PLAYER
+// playerRouter.post('/', function (req, res) {
+//   playerAPI.addNewPlayer(req.body)
+//     .then(() => {
+//       res.redirect('/players');
+//     })
+//     .catch(res.send)
+
+// })
+// // UPDATE SINGLE PLAYER
+// playerRouter.put('/:playerId', function (req, res) {
+//   playerAPI.updatePlayer(req.params.playerId, req.body)
+//     .then(() => {
+//       res.redirect('/players');
+//     })
+// })
+// //DELETE SINGLE PLAYER
+// playerRouter.delete('/:playerId', function (req, res) {
+//   playerAPI.deletePlayer(req.params.playerId).then(res.redirect('/players'))
+// })
 module.exports = {
     userRouter
   }
