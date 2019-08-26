232eeb6850087321fbac9feb515325bfec336110
Oladipo Osinubi <diposinubi@gmail.com>
2019-08-26 11:11:30 -0400

    deleted stadiums and linked teams to users

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,17 @@
 const express = require('express')
 
-const userAPI = require('../models/team.js')
-const playerAPI = require('../models/player.js')
-const userAPI = require('../models/user.js')
+const teamsAPI = require('../models/team.js')
+// const playerAPI = require('../models/player.js')
+const usersAPI = require('../models/user.js')
 
 
 const userRouter = express.Router()
-
+//ZZZZZZZZZZZZZ USER CONTROLLER ZZZZZZZZZZZZ
+// ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
 //GET ALL USERS
 userRouter.get('/', function (req, res) {
-    userAPI.getAllUsers().then(user => {
-        res.render('users/allUsers')
+    usersAPI.getAllUsers().then(users => {
+        res.render('users/allUsers', {users} )
     })
 })
 // CREATE NEW USER
@@ -20,34 +21,80 @@ userRouter.get('/new', function(req, res) {
   
   // EDIT SINGLE USER
   userRouter.get('/:userId/editUser', function(req, res) {
-    userAPI.getUser(req.params.userId).then(user => {
+    usersAPI.getUser(req.params.userId).then(user => {
       res.render('users/editUser', {user})
     })
   })
    
   // GET SINGLE USER
   userRouter.get('/:userId', function(req, res) {
-    userAPI.getUser(req.params.userId).then (user =>{
+    usersAPI.getUser(req.params.userId).then (user =>{
       res.render('users/user', {user})
     })
   })
   //ADD NEW USER
   userRouter.post('/', function(req, res){
-    userAPI.addNewUser(req.body).then(() => {
+    usersAPI.addNewUser(req.body).then(() => {
       res.redirect('/users');
     })
   })
   // UPDATE USER
   userRouter.put('/:userId', function (req, res){
-    userAPI.updateUser(req.params.userId, req.body).then (
+    usersAPI.updateUser(req.params.userId, req.body).then (
       () => {
         res.redirect('/users');
     })
   })
   // DELETE USER
   userRouter.delete('/:userId', function(req, res) {
-    userAPI.deleteUsers(req.params.userId).then(res.redirect('/users'))
+    usersAPI.deleteUsers(req.params.userId).then(res.redirect('/users'))
   })
+
+  // ZZZZZZZZZZZZZZZ TESAM CONTROLLER ZZZZZZZZZZZZZ
+  // ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+  //  GET ALL TEAMS
+userRouter.get('/:userId/teams', function (req, res) {
+  teamsAPI.getAllTeams().then(teams => {
+    res.render('teams/allTeams', {teams})
+  })
+})
+
+// CREATE NEW TEAM
+userRouter.get('/:userId/teams/new', function(req, res) {
+  res.render('teams/newTeam')
+})
+
+// // EDIT SINGLE TEAM
+// userRouter.get('/:teamId/editTeam', function(req, res) {
+//   teamsAPI.getTeam(req.params.teamId).then(team => {
+//     res.render('teams/editTeam', {team})
+//   })
+// })
+ 
+// // GET SINGLE TEAM
+// userRouter.get('/:teamId', function(req, res) {
+//   teamsAPI.getTeam(req.params.teamId).then (team =>{
+//     res.render('teams/team', {team})
+//   })
+// })
+// //ADD NEW TEAM
+// userRouter.post('/', function(req, res){
+//   teamsAPI.addNewTeam(req.body).then(() => {
+//     res.redirect('/teams');
+//   })
+// })
+// // UPDATE TEAM
+// userRouter.put('/:teamId', function (req, res){
+//   teamsAPI.updateTeam(req.params.teamId, req.body).then (
+//     () => {
+//       res.redirect('/teams');
+//   })
+// })
+// // DELETE TEAM
+// userRouter.delete('/:teamId', function(req, res) {
+//   teamsAPI.deleteTeam(req.params.teamId).then(res.redirect('/teams'))
+// })
+
 
 module.exports = {
     userRouter
