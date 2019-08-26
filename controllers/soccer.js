4c8a80e77d1803ae5607aea77ac92c96f1cecd16
Oladipo Osinubi <diposinubi@gmail.com>
2019-08-26 09:15:48 -0400

    made soccer controller

---
+++ controllers/soccer.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+const teamsAPI = require('../models/team.js')
+const playerAPI = require('../models/player.js')
+const stadiumAPI = require('../models/stadium.js')
+
+
+const soccerRouter = express.Router()
